7a2bcfa145c7e50654fcb365de0e571da1a875af
cirowhois <ciro.ruiz95@gmail.com>
2025-02-23 19:47:28 -0300

    Add initial project structure with data ingestion and utility functions

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # etl-criminalidade-sp
+
+# Projeto Aplicado
+
+### Fontes:
+- SSPSP: https://www.ssp.sp.gov.br/estatistica/consultas 
+- IBGE: ['Malha de Setores Censitários - Censo 2022'](https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_de_setores_censitarios__divisoes_intramunicipais/censo_2022/setores/gpkg/BR/)
+
+### Ambiente:
+```
+- Base
+    - Spark 3.5.3/Scala 2.12
+    - Python 3.12.3
+- Requirements
+    - apache-sedona==1.7.0
+    - fiona==1.10.1
+    - folium==0.19.2
+    - geopandas==1.0.1
+    - keplergl==0.3.2
+    - matplotlib==3.10.0
+    - npm==0.1.1
+    - numpy==1.26.4
+    - pandas==1.5.3
+    - pyproj==3.7.0
+    - pyspark==3.5.3
+    - tqdm==4.67.1
+    - wget==3.2
+    - openpyxl==3.1.5
+    - pyarrow==19.0.1
+```
+### Estrutura:
+- src
+    - data
+        - raw 
+
+            _Dado original conforme presente na fonte (formato, colunas, estrutura no geral)_
+        - dispatch 
+
+            _Dado com a estrutura original, mas salvo em .geoparquet, com alguns filtros e definição da coluna geom_
+        - processed 
+
+            _Dado com a estrutura e tratamentos finais, índices e tudo mais proposto no projeto para abrir com o kepler.gl_
+    - etl
+        - ingestion
+            - `ibge.py`
+            - `sspsp.py`
+        - transformation _(em construção)_
+    - `config.json` 
+
+        _Configurações do projeto_
+    - `utils.py` 
+
+        _Pequena lib de apoio_
+    - `ingestion.py` _(em construção)_
+
+        _Apoio para ingestão/download dos dados de base_
+- `requirements.txt`
+- `setupe.sh`
+
+    _Configura o ambiente com as bibliotecas necessárias_
---
+++ src/etl/ingestion/sspsp.py
@@ -0,0 +1,99 @@
+import requests
+import logging
+import os
+import pandas as pd
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+logging.basicConfig(level=logging.INFO)
+
+
+class SSPSPIngestion:
+
+    def __init__(self,json_config) -> None:
+        self.__json_config = json_config
+
+    def load_config(self,json):
+        name = json['name']
+        years = json['source']['years']
+        ref_url = json['source']['url']
+        path = json['datalake_zones']['raw']['path']
+        return name, years, ref_url, path
+       
+    def download(self,url,path)->None:
+        try:
+            file_name = url.split('/')[-1]
+            logging.info(f"Dowloading criminal data for {file_name}")
+            os.makedirs(path, exist_ok=True)
+
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                "Referer": "https://www.ssp.sp.gov.br/"
+                }
+            session = requests.Session()
+            session.headers.update(headers)
+            retries = Retry(total=26, backoff_factor=10, 
+                            status_forcelist=[500, 502, 503, 504],
+                            allowed_methods=["HEAD", "GET", "OPTIONS"])
+            adapter = HTTPAdapter(max_retries=retries)
+            session.mount("https://", adapter)
+            session.mount("http://", adapter)
+            response = session.get(url,stream=True)
+            if response.status_code == 200:
+                with open(f"{path}/{file_name}", 'wb') as f:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+                logging.info(f"Data {file_name} downloaded")
+            else:
+                logging.error(f"Error to download data {response.status_code}")
+                return
+        except Exception as error:
+            logging.error("Error to download data ",error)
+            return
+            #raise Exception("Error to download data ",error)
+
+    def download_all(self):
+        for dict in self.__json_config:
+            name, years, ref_url, path = self.load_config(dict)
+            logging.info(f"Downloading SSPSP data for {name}")
+            for year in years:
+                logging.info(ref_url.format(year))
+                self.download(ref_url.format(year),path)
+
+class SSPSPcsv:
+    def __init__(self,json_config) -> None:
+        self.__json_config = json_config
+
+    def load_config(self,json):
+        name = json['name']
+        path = json['datalake_zones']['raw']['path']
+        sheet_all = json['datalake_zones']['raw']['sheets_all']
+        return name, path, sheet_all
+    
+    def xlsx_list(self,path):
+        file_list = [file for file in os.listdir(path) if file.endswith(".xlsx")]
+        return [os.path.join(path, file) for file in file_list]
+    
+    def read_xlsx(self,file,sheet_all):
+        if sheet_all == True:
+            dict = pd.read_excel(file, sheet_name=None)
+            data = pd.concat(dict.values(), ignore_index=True)
+            return data
+        else:
+            sheet = pd.ExcelFile(file).sheet_names[0]
+            return pd.read_excel(file, sheet_name=sheet)
+        
+    def write_csv(self)->None:
+        
+        for dict in self.__json_config:   
+            logging.info(f"Convert SSPSP data to csv")
+            name, path, sheet_all = self.load_config(dict)
+            logging.info(f"Converting {name}")
+            for file in self.xlsx_list(path):
+                data = self.read_xlsx(file,sheet_all)
+                file_name = file.split('/')[-1].replace('.xlsx','.csv.gz')
+                output_path = f"{path}/{file_name}"
+                data.to_csv(output_path, index=False, compression={'method': 'gzip', 'compresslevel': 6})
+                logging.info(f"Data {file_name} converted")
+
