cf5277860db50b39b2feaad320465273199a15ab
Ciro Ruiz <71043869+cirowhois@users.noreply.github.com>
2025-03-03 18:07:55 -0300

    Update ingestion.py

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,5 +2,4 @@ from etl.ingestion.ibge import IbgeIngestion
 from etl.ingestion.sspsp import SSPSPIngestion
 
 IbgeIngestion('config.json').download_all()
-#SSPSPIngestion('config.json').download_all()
-
+SSPSPIngestion('config.json').download_all()
